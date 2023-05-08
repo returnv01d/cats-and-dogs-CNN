73769bbe24ecb4d5ef3dd541441a226c63df7cc2
return_v01d <b.winiarski97@gmail.com>
2023-05-08 22:21:23 +0200

    Initial commit

---
+++ train.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+import os
+import math
+import tensorflow as tf
+
+
+with tf.device('/cpu:0'):
+
+    train_imagedatagenerator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.0)
+    validation_imagedatagenerator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.0)
+
+    train_iterator = train_imagedatagenerator.flow_from_directory(
+        './input_for_model/train',
+        target_size=(150, 150),
+        batch_size=200,
+        class_mode='binary')
+
+    validation_iterator = validation_imagedatagenerator.flow_from_directory(
+        './input_for_model/validation',
+        target_size=(150, 150),
+        batch_size=50,
+        class_mode='binary')
+
+    print(len(validation_iterator))
+    model = tf.keras.Sequential([
+        # Conv2D + MaxPool2D - convolutional layer
+        # We split the model into three major parts. First, there are three combinations of the Conv2D and MaxPool2D layers.
+        # These are called convolution layers. A Conv2D layer applies a filter to the original image to amplify certain features of the picture.
+        # The MaxPool2D layer reduces the size of the image and reduces the number of needed parameters needed.
+        # Reducing the size of the image will increase the speed of training the network.
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPool2D((2, 2)),
+
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPool2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPool2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    log_dir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    os.makedirs(f"{log_dir}", exist_ok=True)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    history = model.fit(train_iterator,
+                        validation_data=validation_iterator,
+                        steps_per_epoch=100,
+                        epochs=5,
+                        validation_steps=100,
+                        callbacks=[tensorboard_callback])
